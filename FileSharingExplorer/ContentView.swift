a904dca2cdff32152f51b138404691d17f7ebeeb
Bennett Smith <bennettsmith@mac.com>
2024-05-20 10:34:08 -0700

    chore: Adds Xcode Cloud support.

--- FileSharingExplorer/ContentView.swift
+++ FileSharingExplorer/ContentView.swift
@@ -14,6 +14,19 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Read and Write") {
+                let data = Data("Test Message".utf8)
+                let url = URL.documentsDirectory.appending(path: "message.txt")
+                
+                do {
+                    try data.write(to: url, options: [.atomic, .completeFileProtection])
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
         }
         .padding()
     }

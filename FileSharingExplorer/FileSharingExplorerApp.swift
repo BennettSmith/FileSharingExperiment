7725065e7b2a9f86ea533850bfa99fdf48dbd843
Bennett Smith <bennettsmith@mac.com>
2024-05-20 07:57:35 -0700

    Initial Commit

---
+++ FileSharingExplorer/FileSharingExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileSharingExplorerApp.swift
+//  FileSharingExplorer
+//
+//  Created by Bennett Smith on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FileSharingExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

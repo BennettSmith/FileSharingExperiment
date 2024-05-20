fcd5712da953875f567976a2eff9c02a5a43be3e
Bennett Smith <bennettsmith@mac.com>
2024-05-20 08:10:35 -0700

    chore: Create project for file sharing experiments

---
+++ FileSharingExplorer/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  FileSharingExplorer
+//
+//  Created by Bennett Smith on 5/20/24.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Your code here")
+        return true
+    }
+}
